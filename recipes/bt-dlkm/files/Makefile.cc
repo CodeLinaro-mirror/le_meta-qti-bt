2d88dcaf63e845d82ad1a5fff30501db605efd6e
Kuichu Ni <quic_kuichun@quicinc.com>
2022-07-08 23:38:54 +0800

    bt-dlkm: Refine the recipe of bt-dlkm
    1. To avoid a build error "ERROR: Kernel configuration is invalid." is
       output in build log, remove KBDIR and EXTRA_OEMAKE in makfile and bb.
       Use KERNEL_SRC in makefile that is used for building the out-of-tree
       driver.
    
    2. Use clean to replace rm *.o etc in makefile.
    
    Change-Id: I8ca9ed431172f6bc28a7067a8161a9be06dfa36e

--- recipes/bt-dlkm/files/Makefile.cc
+++ recipes/bt-dlkm/files/Makefile.cc
@@ -1,17 +1,12 @@
-KBDIR ?= /lib/modules/$(shell uname -r)/build
-$(info KBDIR=${KBDIR})
-
 obj-m += btpower.o
 
 SRC := $(shell pwd)
 
 all:
-	$(MAKE) -C ${KBDIR} M=${SRC}
+	$(MAKE) -C $(KERNEL_SRC) M=${SRC} modules
 
 modules_install:
-	$(MAKE) -C ${KBDIR} M=${SRC} modules_install
+	$(MAKE) -C ${KERNEL_SRC} M=${SRC} modules_install
 
 clean:
-	rm -f *.o *~ core .depend .*.cmd *.ko *.mod.c
-	rm -f Module.markers Module.symvers modules.order
-	rm -rf .tmp_versions Modules.symvers
+	$(MAKE) -C $(KERNEL_SRC) M=${SRC} clean
