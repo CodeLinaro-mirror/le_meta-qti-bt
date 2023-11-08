603562b4bcf916c0ca3016df7a1e92f17a012411
Venkatesh Sharma <quic_venkshar@quicinc.com>
2023-11-08 08:37:47 +0530

    Service file of bt-app added to daemonize the process
    -Necessary scripts added to set the ownership of bt
     related directories and dev nodes
    
    CRs-Fixed: 3659219
    Change-Id: I3f53ca7b8f68bf9cf99cf3a7da1c38c88e42dc7a

---
+++ recipes/bt-app/bt-app/bt-app-etc.sh
@@ -0,0 +1,7 @@
+#Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+#SPDX-License-Identifier: BSD-3-Clause-Clear
+
+#!/bin/sh
+/bin/mkdir -p /etc/bluetooth
+/bin/chown bluetooth:bluetooth /etc/bluetooth/
+/bin/chown bluetooth:bluetooth /etc/bluetooth/*
